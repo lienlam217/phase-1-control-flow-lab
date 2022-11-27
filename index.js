677af794c12e0fe44d86ae1e6cbdae340f0be4e9
Lien Nguyen <lienlam217@gmail.com>
2022-11-27 15:24:57 -0600

    commit tree 020baeff6c81ad5a18c58d91d9c7603557280127

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(d) {
+ let result
+  if (d <= 400) {
+    result = 'This one is on me!'
+    return result
+  } else if (d <= 2000) {
+    result = 'That will be twenty bucks.'
+    return result 
+  } else if (d <= 2500) {
+    result = 'I will gladly take your thirty bucks.'
+    return result
+  } else result = 'No can do.'
+    return result
+}
+scuberGreetingForFeet(199)
+
+function ternaryCheckCity(city) {
+  
+  return (city === "NYC"? 'Ok, sounds good.' : 'No go.')
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+
+function switchOnCharmFromTip(action) {
+    
+  switch(action) {
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'
+
+
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+ 
